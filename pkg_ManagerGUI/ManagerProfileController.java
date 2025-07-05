811b46a10b3888e4c333c4bc2364dff48f0ce787
SajidaDahir <sajidadahirsheikh@gmail.com>
2025-07-05 13:53:53 +0300

    Updated Manager Profile

--- pkg_ManagerGUI/ManagerProfileController.java
+++ pkg_ManagerGUI/ManagerProfileController.java
@@ -6,61 +6,66 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 import pkg_classes.Manager;
 
 import java.io.IOException;
 
 public class ManagerProfileController {
-    // The Manager object passed from login
+
     private Manager manager;
 
-
-
-    // This method will be called by LoginController to pass the logged-in manager
     public void setManager(Manager manager) {
         this.manager = manager;
-
     }
-
 
     @FXML
     private void handleLogout(ActionEvent event) {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/pkg_Login/Login.fxml"));
-            Parent loginRoot = loader.load();
-
-            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            stage.setScene(new Scene(loginRoot));
-            stage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        loadScene("/pkg_Login/Login.fxml", "Login", event);
     }
+
     @FXML
     private void handleAllocateRoom(ActionEvent event) {
-        try {
-            FXMLLoader loader = new FXMLLoader(
-                    getClass().getResource("/pkg_ManagerGUI/RoomAllocation.fxml")
-            );
-            Parent allocRoot = loader.load();
+        loadScene("/pkg_ManagerGUI/RoomAllocation.fxml", "Allocate Rooms", event);
+    }
 
-            // (Optional) pass data to the allocation controller here:
-            // RoomAllocationController ctrl = loader.getController();
-            // ctrl.setManager(this.manager);
+    @FXML
+    private void handleAddRoom(ActionEvent event) {
+        loadScene("/pkg_ManagerGUI/AddRoom.fxml", "Add Room", event);
+    }
+
+    @FXML
+    private void handleDeleteRoom(ActionEvent event) {
+        loadScene("/pkg_ManagerGUI/DeleteRoom.fxml", "Delete Room", event);
+    }
+
+
+    //  Reusable method to switch scenes safely.
+
+    private void loadScene(String fxmlPath, String title, ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
+            Parent root = loader.load();
 
             Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            stage.setScene(new Scene(allocRoot));
-            stage.setTitle("Allocate Rooms");
+            stage.setScene(new Scene(root));
+            stage.setTitle(title);
             stage.show();
         } catch (IOException e) {
+            showAlert(Alert.AlertType.ERROR, "Navigation Error", "Unable to load page: " + title + "\n" + e.getMessage());
             e.printStackTrace();
         }
     }
 
 
+    // Displays an alert dialog.
 
-
-
-
+    private void showAlert(Alert.AlertType type, String title, String message) {
+        Alert alert = new Alert(type);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
 }
